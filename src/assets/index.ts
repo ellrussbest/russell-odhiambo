ef1cb8aea0343aba0af4faad765695e6a65314ce
ellrussbest <ellrussbest@gmail.com>
2023-12-11 07:32:48 +0100

    whatever happens

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import herobg from "./hearobg";
+import logo from "./logo";
+import close from "./close.svg"
+import menu from "./menu.svg"
+
+export {
+    herobg,
+    logo,
+    close,
+    menu,
+}
