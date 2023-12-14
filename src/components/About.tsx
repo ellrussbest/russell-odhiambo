b82d872dd49143fdd3c67cc34a4317fb33b02f0d
Rusell Odhiambo <Rusell.Odhiambo@cascination.com>
2023-12-14 13:56:00 +0100

    Finished experience section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -35,37 +35,33 @@ const ServiceCard: FC<{index: number, title: string, icon: string}> = ({ index, 
 
 const About = () => {
   return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+    <SectionWrapper idName={"about"}>
+      <>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </motion.div>
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
-        I am a proficient Computer Science Masters student with valuable industry exposure. 
-        My expertise spans across a range of programming languages including C++, QML, Python, and Robot Operating System. 
-        I have also delved into the exciting world of web3 development, utilizing Solidity to create innovative applications. 
-        Furthermore, my hands-on experience in Typescript, React, and NodeJs has allowed me to craft dynamic MERN stack applications. 
-        Recently, I have been captivated by the realm of 3D modeling, particularly through the utilization of WebGL and the Three.js library. 
-        I am eager to further explore and leverage this captivating technology to its fullest potential.
-      </motion.p>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          I am a proficient Computer Science Masters student with valuable industry exposure. 
+          My expertise spans across a range of programming languages including C++, QML, Python, and Robot Operating System. 
+          I have also delved into the exciting world of web3 development, utilizing Solidity to create innovative applications. 
+          Furthermore, my hands-on experience in Typescript, React, and NodeJs has allowed me to craft dynamic MERN stack applications. 
+          Recently, I have been captivated by the realm of 3D modeling, particularly through the utilization of WebGL and the Three.js library. 
+          I am eager to further explore and leverage this captivating technology to its fullest potential.
+        </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div>
-    </>
+        <div className='mt-20 flex flex-wrap gap-10'>
+          {services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+        </div>
+      </>
+      </SectionWrapper>
   );
 };
 
-export default function F(){
-  return  (
-    <SectionWrapper idName={"about"}>
-      <About />
-    </SectionWrapper>
-  )
-}
+export default About
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,8 @@ import web from "../assets/web.png"
 import mernStack from "../assets/mernStack.png"
 import reactNative from "../assets/reactNative.png"
 import ros from "../assets/ROS.png"
+import kemriCDC from "../assets/kemriCDC.jpg"
+import cascination from "../assets/cascination.png"
 
 
 export const navLinks = [
@@ -39,5 +41,52 @@ export const navLinks = [
     {
       title: "Robot Operating System Developer",
       icon: ros,
+    },
+  ];
+
+  export const experiences = [
+    {
+      title: "IT Attachee",
+      company_name: "KEMRI-CDC, Nairobi Kenya",
+      icon: kemriCDC,
+      iconBg: "#383E56",
+      date: "May 2022 - July 2022",
+      points: [
+        "I maintained database servers by regularly checking for data errors and hardware defaults\
+        ensuring uninterrupted access to the company's information. This increased the company's \
+        efficiency and reliability.",
+
+        "I troubleshot and monitored the company's network using Wireshark.\
+        I played a crucial role in identifying and resolving potential issues\
+        thus increasing security, minimizing downtime and optimizing network efficiency",
+
+        "I automated tasks through Batch and Bash scripting thus increasing operational\
+        efficiency and reducing manual effort. This automation optimized workflow process and also minimized the\
+        potential for human error ultimately improving reliablity of the company's system",
+      ],
+    },
+    {
+      title: "IT and Software Developer Intern",
+      company_name: "CASCINATION, Bern Switzerland",
+      icon: cascination,
+      iconBg: "#383E56",
+      date: "February 2023 - Februrary 2024",
+      points: [
+        "I developed a package creator automation software using Python and QML which significantly enhanced the company's operational effieicency.\
+        This software played a vital role in generating essential packages for the client-side application installer and updater,\
+        thereby improving company's standard procedures.",
+
+        "I developed FileMaker custom plugin using C++, Javascript and WebAssembly. This plugin was instrumental in generating and stroing EEPROM strings\
+        for the comany's medical device firmware thereby enhancing the product's functionality.",
+
+        "I played a key role in enhancing the company's operational efficiency by developing and troubleshooting Filemaker application to maintain the\
+        company's ERP.",
+
+        "I Collaborated with cross-functional teams including designers, product managers, and other developers to create high-quality and regulated software.",
+
+        "I learned and developed Simple Robot operating system application",
+
+        "I contributed in debugging and refactoring of the existing code base.",
+      ],
     },
   ];
